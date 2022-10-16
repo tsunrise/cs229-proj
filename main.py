1e5cfb57543b813fbe959f0fc2a66409acb04a39
Conghao Shen <me@tomshen.io>
2022-10-16 14:36:08 -0700

    feat: init commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import utils.devices as devices
+
+if __name__ == '__main__':
+    devices.status_check()
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ preprocess/fetch.py
@@ -0,0 +1,62 @@
+import os
+import requests
+from tqdm import tqdm
+import toml
+import tarfile
+import shutil
+from dataclasses import dataclass
+
+DATASETS_CONFIG_PATH = "datasets.toml"
+CACHE_DIR = ".cs229_cache"
+
+def download(filename: str, url: str) -> str:
+    """Download the tar file from crate.io, and output the path to the tar file."""
+    if not os.path.exists(CACHE_DIR):
+        os.makedirs(CACHE_DIR)
+    cache_path = os.path.join(CACHE_DIR, filename)
+    if not os.path.exists(cache_path):
+        with requests.get(url, stream=True) as r:
+            total_size_in_bytes = int(r.headers.get('content-length', 0))
+            chunk_size = 1024
+            with open(cache_path, "wb") as f:
+                print(f"Downloading {filename} from {url}")
+                progress = tqdm(total = total_size_in_bytes, unit = 'iB', unit_scale = True)
+                for chunk in r.iter_content(chunk_size=chunk_size):
+                    progress.update(len(chunk))
+                    f.write(chunk)
+                progress.close()
+    else:
+        print("Using cached: ", filename)
+    return cache_path
+
+@dataclass
+class CratesIOCSVPath:
+    categories: str
+    crates: str
+    crates_categories: str
+    dependencies: str
+
+def dump_crate_io() -> CratesIOCSVPath:
+    """Download the tar file from crate.io, and output the path to the csv data."""
+    config = toml.load(DATASETS_CONFIG_PATH)
+    tar_path = download("crates-io.tar.gz", config["crates-io"]["url"])
+    # extract the tar file
+    crates_io_path = os.path.join(CACHE_DIR, "crates-io")
+    if os.path.exists(crates_io_path):
+        print("Cleaning up old crates-io data")
+        shutil.rmtree(crates_io_path)
+    os.makedirs(crates_io_path)
+    print("Extracting crates-io data")
+    with tarfile.open(tar_path) as tar:
+        paths = {}
+        for info in tqdm(tar.getmembers(), unit="files"):
+            filename = os.path.basename(info.name)
+            if filename.endswith(".csv"):
+                info.name = os.path.basename(info.name)
+                tar.extract(info, crates_io_path)
+                paths[filename] = os.path.join(crates_io_path, info.name)
+    return CratesIOCSVPath(paths["categories.csv"], paths["crates.csv"], paths["crates_categories.csv"], paths["dependencies.csv"])
+
+if __name__ == "__main__":
+    crates_io_path = dump_crate_io()
+    print("Crates.io data is at: ", crates_io_path)
