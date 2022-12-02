c421cd1979ebffcb1ef2402d4643d8026e84cefb
Conghao Shen <me@tomshen.io>
2022-12-02 14:28:59 -0800

    chore: update rust interface

---
+++ md2txt/md2txt.pyi
@@ -0,0 +1,10 @@
+from typing import List
+
+
+def markdown_to_text(markdown: str) -> str:
+    """Converts a markdown string to a text string."""
+    ...
+
+def batch_markdown_to_text(markdowns: List[str]) -> List[str]:
+    """Converts a list of markdown strings to a list of text strings."""
+    ...
